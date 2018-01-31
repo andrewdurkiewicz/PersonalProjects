d2b869802bd3cc9443baad95d484d1525c9c3900
Andrew <andrewdurkiewicz@gmail.com>
2018-01-30 21:04:54 -0700

    pushing new python doc

---
+++ IEE380/distributions.py
@@ -0,0 +1,12 @@
+import numpy as np
+import math
+def binomial(n,x):
+    return math.factorial(n)/(math.factorial(n-x)*math.factorial(x))
+def cumulative(n,p,x):
+    return binomial(n,x) * (p)**(x) * (1-p)**(n-x)
+x = np.arange(0,4,1)
+result = 0
+for i in x:
+    result+=cumulative(3,.5,i)
+    print(result.as_integer_ratio())
+    
